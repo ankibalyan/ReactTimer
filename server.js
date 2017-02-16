3a41cba52afacdf9db370edc8efa227a2d5d939a
Ankit Kumar <ankit@ipapagari.com>
2017-02-16 13:50:21 +0530

    local server fix

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 
 const PORT = 9000;
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+  next();
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
